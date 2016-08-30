f83b1bf4a53d3e8a727f2700a3ca13057e218130
AnEpicName <matiashern1321@gmail.com>
2016-08-30 15:16:16 -0300

    Agregado un widget básico

---
+++ app/src/main/java/cl/jgutierrez/android/menuexample/WidgetControl.java
@@ -0,0 +1,17 @@
+package cl.jgutierrez.android.menuexample;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * Created by matias on 30-08-16.
+ */
+
+public class WidgetControl extends AppWidgetProvider{
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
